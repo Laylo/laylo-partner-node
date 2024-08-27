3e349640e31bcaa74ed5b11406ed85625a802861
Scott Schwartz <scott@laylo.com>
2024-08-27 09:30:58 -0700

    remove process from public access

--- src/testServer/index.ts
+++ src/testServer/index.ts
@@ -2,9 +2,9 @@ import * as http from "http";
 import { conversions, config } from "../index";
 
 config({
-  id: "LAYLO_ACCOUNT_ID",
-  accessKey: "LAYLO_ACCESS_KEY",
-  secretKey: "LAYLO_SECRET_KEY",
+  id: "AGxl96OBmWykueJT206Kxjw5X",
+  accessKey: "9fd6448302244cfc8644de856832e91d",
+  secretKey: "732ebbbed39c4226b8032ba11858edc1",
 });
 
 const PORT = 3000;
@@ -18,11 +18,12 @@ const server = http.createServer(async (req, res) => {
   if (req.url === "/track") {
     const value = async () => {
       const value = await conversions.track({
-        customerApiKey: "A_CUSTOMERS_API_KEY",
-        name: "TOUR",
+        customerApiKey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklkIjoiZjczZjY0MTgtNDhhZS00MWUxLWFjMDEtYTYyNzk0ODhmODk2IiwidXNlcklkIjoibkxZeHc2U2MwS0lkTDlFM3JqUHZUbEJjZiIsInRva2VuVHlwZSI6IlNVQlNDUklCRSIsImhhc0NsaWVudEFjY2VzcyI6dHJ1ZSwiY3JlYXRlZCI6MTcyNDY5NTM1NTE5OCwibGFiZWwiOiJjb252ZXJzaW9ucyIsImlhdCI6MTcyNDY5NTM1NX0.NCNHm0IHuug-dxOp-atiRboz2sXYeYG7s6Sy4jZnaIo",
+        name: "TOUR3",
         action: "PURCHASE",
-        metadata: { title: "EVENT_ID", value: 100 },
-        user: { phone: "+1111111111", marketingConsent: true },
+        metadata: { title: "EVENT_ID" },
+        user: { phone: "+13104972709", marketingConsent: true },
       });
 
       console.log(value);
